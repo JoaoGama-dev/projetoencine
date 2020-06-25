39549f3122380605fc43205d9585fa83206c750e
JoaoGama-dev <joaogamahjj@gmail.com>
2020-06-24 21:05:12 -0300

    animacao-finalizada

--- javascript/script.js
+++ javascript/script.js
@@ -2,16 +2,20 @@ const target = document.querySelectorAll('[data-animacao]');
 const animacaoclass = 'animacao';
 
 function animeScroll() {
-    const windowTop = window.pageYOffset;
+    const windowTop = window.pageYOffset + ((window.innerHeight*3)/4);
     target.forEach(function(element) {
       if((windowTop) > element.offsetTop) {
           element.classList.add(animacaoclass);
+        } else {
+            element.classList.remove(animacaoclass);
         }
-
-    console.log(element.offsetTop);
     })
 }
 
-window.addEventListener('scroll', function() {
+animeScroll();
+
+if(target.length) {
+    window.addEventListener('scroll', function() {
     animeScroll();
-})
+    })
+}
